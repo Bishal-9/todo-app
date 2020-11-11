b8f93b7f16e8550b2cc643c797b618aa789c49e6
Bishal Goon <goonbishal.bg@gmail.com>
2020-11-11 20:53:09 +0530

    Todo App done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core/';
+import Todo from './Todo';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('tasks').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
+    })
+  })
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    db.collection('tasks').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setInput('');
+  }
+
   return (
-    <div className="App">
-      <h1>Here's my TODO APP</h1>
+    <div className="app">
+      
+      <form>
+        <FormControl>
+          <InputLabel>Write a TODO</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
+
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>       
+      </form>
+
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo} />
+        ))}
+      </ul>
     </div>
   );
 }
